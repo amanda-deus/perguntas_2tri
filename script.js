4e397cda529ad614c1e5957481341504ef02dea4
amanda <138232841+amanda-deus@users.noreply.github.com>
2025-07-02 12:18:05 -0300

    .

--- script.js
+++ script.js
@@ -6,41 +6,41 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Ao fazer compras no supermercado, qual das seguintes atitudes melhor te descreve?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "afirmação"
+                texto: "Comprador(a) Consciente!",
+                afirmacao: "Você costuma fazer um lista antes de sair de casa e se esforça para comprar apenas o necessário, evitando promoções que te levariam a adquirir mais do que realmente precisa. Sua despensa e geladeira são bem organizadas e você sabe o que tem."
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "afirmação"
+                texto: "Comprador(a) Impulsivo(a)!",
+                afirmacao: "Vocẽ se sente atraído(a) por ofertas e muitas vezes compra mais do que planejava, especialmente itens que talvez não use antes que estraguem. Sua geladeira e despensa podem ter alguns 'tesouros escondidos' que você esqueceu."
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Sobras de refeições são um desafio para você?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "afirmação"
+                texto: "Mestre das sobras.",
+                afirmacao: "Você adora transformar sobras em novas e deliciosas refeições, ou as congela para cotumo posterior. Para você, nada se perde, tudo se transforma."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "afirmação"
+                texto: "Despreocupado(a) com Sobras.",
+                afirmacao: "Você tende a jogar fora as sobras que não foram consumidas imediatamente. A ideia de 'dar um jeito' nas sobras não é algo que te ocorre com frequência."
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Quando percebe que um alimento está próximo da validade, qual é a sua reação?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "afirmação"
+                texto: "Otimizador(a) de Alimentos.",
+                afirmacao: "Vocẽ imediatamente pensa em formas de usar aquele alimento, seja cozinhando, congelando ou oferecendo a alguém que possa consumi-lo a tempo."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "afirmação"
+                texto: "Descartador(a) Rápido(a).",
+                afirmacao: "Você tende a jogar fora o alimentos que estão perto do vencimento por preucação,mesmo que ainda pareça bons para consumo."
             }
         ]
     },
